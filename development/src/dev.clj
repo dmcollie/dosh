d1dd9b2be5411cefc8a8cfc555c4f199e16d8116
David Collie <dmcollie@me.com>
2022-06-06 23:22:09 +0100

    Improved formatting.

--- development/src/dev.clj
+++ development/src/dev.clj
@@ -1,4 +1,8 @@
 (ns dev
   (:require [lazd.dosh.domain.model.interface :as model]))
 
-(prn (model/validate-account {:name "D M COLLIE"}))
+(def fred-account {:name           "Fred Dibnah"
+                   :sort-code      "12-34-56"
+                   :account-number "12345678"})
+
+(model/validate-account fred-account)

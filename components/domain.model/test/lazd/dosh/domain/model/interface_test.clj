4845882d75122eb0ba597dc2a35e513451067011
David Collie <dmcollie@me.com>
2022-06-06 23:10:34 +0100

    Test check-account.

--- components/domain.model/test/lazd/dosh/domain/model/interface_test.clj
+++ components/domain.model/test/lazd/dosh/domain/model/interface_test.clj
@@ -1,6 +1,8 @@
-(ns lazd.dosh.schema.interface-test
+(ns lazd.dosh.domain.model.interface-test
   (:require [clojure.test :as test :refer :all]
-            [lazd.dosh.schema.interface :as schema]))
+            [lazd.dosh.domain.model.interface :as model]))
 
 (deftest dummy-test
-  (is (= 1 1)))
+  (is (model/validate-account {:name "Fred Dibnah"
+                               :sort-code "12-34-56"
+                               :account-number "12345678"})))
